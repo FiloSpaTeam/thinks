2ccd0083101ed18e1798b98e458d93115c9978a5
Claudio Maradonna <claudiom@keyson.it>
2015-10-05 19:34:46 +0200

    Select start page images. Fixed error class handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active_link_if_current(path)
     return "active" if current_page?(path)
   end
+
+  def set_has_error_form_group(resource, name)
+    'has-error' if resource.errors.include?(name)
+  end
 end
