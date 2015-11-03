f69e3df1581dd7e9f31d1336a9a64253f27112a3
Claudio Maradonna <claudiom@keyson.it>
2015-11-03 19:08:56 +0100

    Changed comment view. Added likes count. Now need to avoid ourself to vote our comment and vote for workload if we did not vote or promote a solution

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,12 @@
 module LikesHelper
+  def like_count(number)
+    case number
+    when 0..20 then number
+    when 20..50 then "20+"
+    when 50..100 then "50+"
+    when 100..500 then "100+"
+    when 500..1000 then "500+"
+    when 1000..Float::INFINITY then "1000+"
+    end
+  end
 end
