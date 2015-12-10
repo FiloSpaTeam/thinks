a13c6ce7d14863aa213d349c12d4c32d270673b0
Claudio Maradonna <claudiom@keyson.it>
2015-12-10 19:48:08 +0100

    Application version updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   require_dependency "modules/redcarpet/render/cleanerhtml"
 
   def application_version
-    version = "0.1.0"
+    version = "0.1.0.0"
     "Version: #{version}"
   end
 
