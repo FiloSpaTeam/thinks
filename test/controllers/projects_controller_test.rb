c5146324d1d933f4d3ced46a4d15602344d1ddff
Claudio Maradonna <penguyman@stronzi.org>
2016-01-07 22:37:14 +0100

    Refactored quotes with single quotes like Syntax guidelines

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -7,18 +7,18 @@ class ProjectsControllerTest < ActionController::TestCase
     authenticate
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:projects)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create project" do
+  test 'should create project' do
     assert_difference('Project.count') do
       post :create, project: { release_at: Date.today, description: "Fantastic project", minimum_team_number: 5, title: "Eureka", license_id: 471458987, home_url: "http://myurl.org/", source_code_url: "http://myurl.org/src", documentation_url: "http://myurl.org/doc", cycle_id: 788802281, category_id: 298486374, thinker_id: 135138680 }
     end
@@ -26,32 +26,26 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should show project" do
+  test 'should show project' do
     get :show, id: @project
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @project
     assert_response :success
   end
 
-  test "should update project" do
+  test 'should update project' do
     patch :update, id: @project, project: { release_at: @project.release_at, description: @project.description, minimum_team_number: @project.minimum_team_number, title: @project.title, license_id: @project.license_id, home_url: @project.home_url, source_code_url: @project.source_code_url, documentation_url: @project.documentation_url }
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should destroy project" do
+  test 'should destroy project' do
     assert_difference('Project.count', -1) do
       delete :destroy, id: @project
     end
 
     assert_redirected_to projects_path
   end
-
-  # test "should check a project that cannot be created in the past" do
-  #   assert_no_difference('Project.count') do
-  #     post :create, project: { release_at: Date.yesterday, description: "Fantastic outdated project", minimum_team_number: 3, title: "Eureka", license_id: 4 }
-  #   end
-  # end
 end
