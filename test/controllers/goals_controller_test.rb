8ecbd264b1142f452dc9adba8cbee58434554196
Claudio Maradonna <penguyman@stronzi.org>
2016-01-07 23:14:26 +0100

    Created tests for goals

--- test/controllers/goals_controller_test.rb
+++ test/controllers/goals_controller_test.rb
@@ -2,44 +2,46 @@ require 'test_helper'
 
 class GoalsControllerTest < ActionController::TestCase
   setup do
-    @goal = goals(:one)
+    @goal = goals(:notification_system)
+
+    authenticate
   end
 
-  test "should get index" do
-    get :index
+  test 'should get index' do
+    get :index, project_id: @goal.project
     assert_response :success
     assert_not_nil assigns(:goals)
   end
 
-  test "should get new" do
-    get :new
+  test 'should get new' do
+    get :new, project_id: @goal.project
     assert_response :success
   end
 
-  test "should create goal" do
+  test 'should create goal' do
     assert_difference('Goal.count') do
-      post :create, goal: {  }
+      post :create, project_id: @goal.project_id, goal: { title: 'Auth system', description: 'All related to authentication system, excluded OAuth!', project_id: @goal.project_id, thinker_id: 135_138_680 }
     end
 
     assert_redirected_to goal_path(assigns(:goal))
   end
 
-  test "should show goal" do
+  test 'should show goal' do
     get :show, id: @goal
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @goal
     assert_response :success
   end
 
-  test "should update goal" do
+  test 'should update goal' do
     patch :update, id: @goal, goal: {  }
     assert_redirected_to goal_path(assigns(:goal))
   end
 
-  test "should destroy goal" do
+  test 'should destroy goal' do
     assert_difference('Goal.count', -1) do
       delete :destroy, id: @goal
     end
--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save goal if title is empty or length is over 60' do
+    goal = goals(:notification_system)
+
+    goal.title = ''
+    assert_not goal.save, 'Saved the goal with an empty title.'
+
+    goal.title = random_string(61)
+    assert_not goal.save, 'Saved the goal with a too long title.'
+  end
+
+  test 'should not save goal if description length is less than 30' do
+    goal = goals(:notification_system)
+
+    goal.description = random_string(29)
+    assert_not goal.save, 'Saved the goal with too short description.'
+  end
 end
