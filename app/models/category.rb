da2fbeaf106da12c368c4884966daf537d629c20
Claudio Maradonna <penguyman@stronzi.org>
2016-01-08 21:17:15 +0100

    Tests for categories done. Working over comments testing

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :projects
 
+  validates :t_name, presence: true
+  validates :t_description, presence: true
+
   def name
-    I18n.translate("category.#{self.t_name}")
+    I18n.translate("category.#{t_name}")
   end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save category without name' do
+    category = categories(:games)
+
+    category.t_name = nil
+
+    assert_not category.save, 'Saved category with empty name'
+  end
+
+  test 'should not save category without description' do
+    category = categories(:games)
+
+    category.t_description = nil
+
+    assert_not category.save, 'Saved category with empty description'
+  end
 end
