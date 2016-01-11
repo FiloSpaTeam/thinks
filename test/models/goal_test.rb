cca5622e4f483a724e28a5872c924d07851c2b6b
Claudio Maradonna <penguyman@stronzi.org>
2016-01-11 22:45:00 +0100

    Test for goal models done

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -17,4 +17,18 @@ class GoalTest < ActiveSupport::TestCase
     goal.description = random_string(29)
     assert_not goal.save, 'Saved the goal with too short description.'
   end
+
+  test 'should not save goal if progress is less than 0' do
+    goal = goals(:notification_system)
+
+    goal.progress = -1
+    assert_not goal.save, 'Saved the goal with progress less than 0.'
+  end
+
+  test 'should not save goal if progress is more than 100' do
+    goal = goals(:notification_system)
+
+    goal.progress = 101
+    assert_not goal.save, 'Saved the goal with progress less than 0.'
+  end
 end
