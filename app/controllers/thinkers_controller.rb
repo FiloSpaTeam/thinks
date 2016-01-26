4334d93df543e31e1291addb1a0926da9cb9cfa4
Claudio Maradonna <penguyman@stronzi.org>
2016-01-26 18:54:37 +0100

    First view of userpage is done.

--- app/controllers/thinkers_controller.rb
+++ app/controllers/thinkers_controller.rb
@@ -11,6 +11,24 @@ class ThinkersController < ApplicationController
     @thinker = Thinker.friendly.find(params[:id])
   end
 
+  def edit
+    @thinker = Thinker.friendly.find(params[:id])
+  end
+
+  def update
+    @thinker = Thinker.friendly.find(params[:id])
+
+    respond_to do |format|
+      if current_thinker == @thinker && @thinker.update(thinker_params)
+        format.html { redirect_to @thinker, notice: 'Your data are updated.' }
+        format.json { render :show, status: :ok, location: @thinker }
+      else
+        format.html { render :edit }
+        format.json { render json: @thinker.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   # At the moment we need to redirect others, later will be there a public page
@@ -20,5 +38,12 @@ class ThinkersController < ApplicationController
 
   def check_admin!
     redirect_to root_url unless current_thinker.try(:admin?)
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def thinker_params
+    allowed_params = [:name, :email]
+
+    params.require(:thinker).permit(allowed_params)
   end
 end
