8cbcf40e674413eaaa71ffcbef4a44ec320a4f01
Claudio Maradonna <penguyman@stronzi.org>
2016-01-28 00:13:38 +0100

    Fixed view for edit and new project. Updated to 0.1.2.50

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   require_dependency 'modules/redcarpet/render/cleanerhtml'
 
   def application_version
-    version = '0.1.2.49'
-    date    = '2016.01.27'
+    version = '0.1.2.50'
+    date    = '2016.01.28'
     "Version: #{version} (build: #{date})"
   end
 
