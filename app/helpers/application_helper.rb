32e7f021789e395d03ff995137b1ce8b7818eebc
Claudio Maradonna <penguyman@stronzi.org>
2016-02-25 20:00:59 +0100

    Updated to 0.1.2.97

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   require_dependency 'modules/redcarpet/render/cleanerhtml'
 
   def application_version
-    version = '0.1.2.96'
+    version = '0.1.2.97'
     date    = '2016.02.25'
     "Version: #{version} (build: #{date})"
   end
