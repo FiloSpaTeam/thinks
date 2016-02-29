059acde7d07bc8bec019513fb5f782e45f609cf0
Claudio Maradonna <penguyman@stronzi.org>
2016-02-29 22:04:13 +0100

    Simplified survey checking.

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -12,7 +12,7 @@ class SprintsController < ApplicationController
       params[:filterrific],
       select_options: {}
     ) || return
-    @sprints = @filterrific.find.page params[:page]
+    @sprints         = @filterrific.find.page params[:page]
   rescue ActiveRecord::RecordNotFound => e
     # There is an issue with the persisted param_set. Reset it.
     puts "Had to reset filterrific params: #{e.message}"
