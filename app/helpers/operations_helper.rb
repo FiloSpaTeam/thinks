225749304cb163dde4ef77c2406a1810ae11fc73
Claudio Maradonna <penguyman@stronzi.org>
2016-03-12 00:49:21 +0100

    Fixed bug with button in operations

--- app/helpers/operations_helper.rb
+++ app/helpers/operations_helper.rb
@@ -1,12 +1,12 @@
 module OperationsHelper
   def done_button(operation)
-    link_to done_operation_path(operation), :class => "pull-right btn", :role => "button", :title => t("done"), method: :put do
+    link_to done_operation_path(operation), :class => "pull-right btn actions", :role => "button", :title => t("done"), method: :put do
       content_tag(:span, "", :class => "glyphicon glyphicon-ok", "aria-hidden" => "true")
     end
   end
 
   def destroy_button(operation)
-    link_to operation_path(operation), :class => "pull-right btn", :role => "button", :title => t("remove"), method: :delete do
+    link_to operation_path(operation), :class => "pull-right btn actions", :role => "button", :title => t("remove"), method: :delete do
       content_tag(:span, '', :class => 'glyphicon glyphicon-remove', 'aria-hidden' => 'true')
     end
   end
