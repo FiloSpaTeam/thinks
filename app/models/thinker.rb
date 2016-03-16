0bbdd53dac9a0795eeebafa365ad3f2109fa94f1
Claudio Maradonna <penguyman@stronzi.org>
2016-03-16 22:46:13 +0100

    Now date of birth is ranged

--- app/models/thinker.rb
+++ app/models/thinker.rb
@@ -17,6 +17,10 @@ class Thinker < ActiveRecord::Base
   has_and_belongs_to_many :teams, class_name: 'Project'
   has_and_belongs_to_many :notifications
 
+  validates_date :born_at, :before => -> { 14.years.ago },
+                 :before_message => 'must be at least 14 years old',
+                 :on_or_after => -> { 100.years.ago }
+
   has_many :contributions
   has_many :projects
   has_many :working_tasks, class_name: 'Task', foreign_key: :worker_thinker_id
