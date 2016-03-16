5c40533417ecd2bb32ed2ec1a45e4a7587823a89
Claudio Maradonna <penguyman@stronzi.org>
2016-03-16 22:21:30 +0100

    Auto expiring session after 1 hour

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rethink_session'
+Rails.application.config.session_store :cookie_store,
+                                       key: '_think_session',
+                                       expire_after: 1.hours
