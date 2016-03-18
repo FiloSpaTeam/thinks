18c68334294f654f076543b30de5a6b9489616ab
Claudio Maradonna <penguyman@stronzi.org>
2016-03-18 20:04:08 +0100

    Fix bug with part_of_team function

--- app/models/project.rb
+++ app/models/project.rb
@@ -75,7 +75,7 @@ class Project < ActiveRecord::Base
   end
 
   def part_of_team?(thinker)
-    return !contributions.select{ |contribution| contribution.thinker == thinker && contribution.partecipate? }.empty? || thinker == self.thinker
+    return thinker == self.thinker || !contributions.select{ |contribution| contribution.thinker == thinker && contribution.partecipate? }.empty?
   end
 
   def progress_percentage
