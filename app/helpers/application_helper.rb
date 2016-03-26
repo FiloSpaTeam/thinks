7c3f5c9cd9a8b1b4db03e22070eea546e8999e4e
Claudio Maradonna <penguyman@stronzi.org>
2016-03-26 12:38:20 +0100

    Text justified in index and show. Updated to 0.1.7.163

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   require_dependency 'modules/redcarpet/render/cleanerhtml'
 
   def application_version
-    version  = '0.1.7.162'
+    version  = '0.1.7.163'
     date     = '2016.03.26'
     thinkers = Thinker.all.size
     content_tag(:span, "(Online: #{thinkers})", class: 'hidden-xs hidden-sm pull-right text-muted') +
