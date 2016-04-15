84ca13152218819bbed8a8f1f7213e9916b07deb
Claudio Maradonna <penguyman@stronzi.org>
2016-04-16 00:13:09 +0200

    Updated to 0.1.10.199

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   require_dependency 'modules/redcarpet/render/cleanerhtml'
 
   def application_version
-    version  = '0.1.10.197'
-    date     = '2016.04.15'
+    version  = '0.1.10.199'
+    date     = '2016.04.16'
     thinkers = Thinker.all.size
     content_tag(:span, "(Subscribers: #{thinkers})", class: 'hidden-xs hidden-sm pull-right text-muted') +
     content_tag(:small, "Version #{version}", class: 'text-muted pull-right', title: date)
