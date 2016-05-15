d2524fadd09487b115ddef073c24770f07027f20
Claudio Maradonna <penguyman@stronzi.org>
2016-05-15 16:00:56 +0200

    Updated files with new License

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+# coding: utf-8
+# This file is part of Thinks.
+
+# Thinks is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Thinks is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+
+# You should have received a copy of the GNU Affero Public License
+# along with Thinks.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright (c) 2015, Claudio Maradonna
+
 # -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
