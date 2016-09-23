d92bf1576b056294880086c2bf32628daf33a9cf
Claudio Maradonna <penguyman@stronzi.org>
2016-09-23 22:50:38 +0200

    New licenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -115,12 +115,14 @@ licenses_list = [
   ['fdlo',           'FDLOther',          'https://www.gnu.org/licenses/fdl.html'],
   ['ccby4',          'CCBY4',             'http://creativecommons.org/licenses/by/4.0/legalcode'],
   ['ccbysa4',        'CCBYSA4',           'http://creativecommons.org/licenses/by-sa/4.0/legalcode'],
+  ['ccbync4',        'CCBYNC4',           'https://creativecommons.org/licenses/by-nc/4.0/legalcode'],
   ['dsl',            'DSL',               'https://www.gnu.org/licenses/dsl.html'],
   ['freeart',        'FreeArt',           'https://directory.fsf.org/wiki/License:Free-Art-L-v1.3'],
   ['odbl',           'ODbl',              'http://directory.fsf.org/wiki/License:ODbl'],
   ['silofl',         'SILOFL',            'http://scripts.sil.org/OFL_web'],
   ['gnuv',           'GNUVerbatim',       'https://www.gnu.org/licenses/licenses.html#VerbatimCopying'],
   ['ccbynd',         'CCBYND3',           'http://creativecommons.org/licenses/by-nd/3.0/legalcode'],
+  ['ccbynd4',        'CCBYND4',           'https://creativecommons.org/licenses/by-nd/4.0/legalcode'],
   ['hpnd',           'HPND',              'http://directory.fsf.org/wiki/License:Historical_Permission_Notice_and_Disclaimer']
 ]
 
