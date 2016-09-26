1bc1c0754d73524588c9615d8fe855454e1a1939
Claudio Maradonna <penguyman@stronzi.org>
2016-09-26 22:59:57 +0200

    Other optimization

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -24,7 +24,7 @@ class ReleasesController < ApplicationController
 
   def index
     @filterrific = initialize_filterrific(
-      Release,
+      Release.includes(:tasks),
       params[:filterrific],
       select_options: {}
     ) || return
