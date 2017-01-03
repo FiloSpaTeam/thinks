d1136475b6a309d2e59cc90e10140c9498c8b734
Claudio Maradonna <penguyman@stronzi.org>
2017-01-03 01:18:12 +0100

    New push

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Rethink
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('node_modules')
+
     config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
