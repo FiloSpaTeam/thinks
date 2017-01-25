5ca7afa2ba136785afb716c1c312a2a8f6c11946
Claudio Maradonna <penguyman@stronzi.org>
2017-01-25 09:38:17 +0100

    Fix for migrations

--- db/migrate/20160111194652_add_column_progress_to_goals.rb
+++ db/migrate/20160111194652_add_column_progress_to_goals.rb
@@ -2,7 +2,7 @@ class AddColumnProgressToGoals < ActiveRecord::Migration
   def change
     add_column :goals, :progress, :float
 
-    goals = Goal.all
+    goals = Goal.unscoped.all
     goals.each do |goal|
       goal.progress = goal.progress_percentage
 
