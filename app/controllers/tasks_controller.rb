8120bf6c0bfd7ae49c01a829591e55c16d3a78a5
Claudio Maradonna <penguyman@stronzi.org>
2017-05-27 10:52:38 +0200

    Now by default tasks done are hidden. But you can search them with filters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,7 @@
 # Copyright (c) 2015, Claudio Maradonna
 
 class TasksController < ApplicationController
+  include ApplicationHelper
   include ProjectsHelper
   include StatusesHelper
   include TasksHelper
@@ -45,10 +46,12 @@ class TasksController < ApplicationController
                   .includes(:status, :thinker, :updater, :goal, :children)
                   .where(project: @project)
                   .where(recruitment: false)
+
+    tasks_scope = tasks_scope.where.not(status: Status.done.first) unless active_filter?(:status_id)
+
     tasks_scope = apply_filters(tasks_scope, params[:filters]) if params[:filters].present?
     tasks_scope = apply_sorter(tasks_scope, params[:tasks_smart_listing][:sort]) if params.key?(:tasks_smart_listing) &&
                                                                                     params[:tasks_smart_listing].key?(:sort)
-
 
     smart_listing_create :tasks,
                          tasks_scope,
