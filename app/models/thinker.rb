acb29ba2748ea2352b7a9cf67e4556ac37e610ec
Claudio Maradonna <penguyman@stronzi.org>
2017-07-12 22:10:41 +0200

    Fixed a BUG difficult to reproduce with a project started without sprints

--- app/models/thinker.rb
+++ app/models/thinker.rb
@@ -37,9 +37,10 @@ class Thinker < ActiveRecord::Base
   has_and_belongs_to_many :notifications
   has_and_belongs_to_many :skills
 
-  validates_date :born_at, :before => -> { 14.years.ago },
-                 :before_message => 'must be at least 14 years old',
-                 :allow_blank => true
+  validates_date :born_at,
+                    :before => -> { 14.years.ago },
+                    :before_message => 'must be at least 14 years old',
+                    :allow_blank => true
 
   has_many :contributions
   has_many :projects
