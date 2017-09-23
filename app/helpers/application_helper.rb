da4e33d12ee6918fa33c0ad1580a03b2369094c2
Claudio Maradonna <penguyman@stronzi.org>
2017-09-23 12:58:12 +0200

    Saving

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,11 +21,8 @@ module ApplicationHelper
   require_dependency 'modules/redcarpet/render/cleanerhtml'
 
   def application_version
-    version  = '0.1.82.1447'
-    date     = '2017.07.03'
-    thinkers = Thinker.all.size
-    content_tag(:span, "(Subscribers: #{thinkers})", class: 'hidden-xs hidden-sm pull-right text-muted') +
-      content_tag(:small, "Version #{version}", class: 'text-muted pull-right hidden-xs ', title: date)
+    version = '0.1.82.1447'
+    content_tag(:div, 'v' + version, style: 'color: #BCBCBC;', class: 'margin-top-50 hidden-xs')
   end
 
   def link_to_button(path, text, icon_source)
